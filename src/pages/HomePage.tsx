62e58a68acb0ec7bf713def0ad8e5cdbf07161f3
seunjin <jinseun@gmail.com>
2025-09-07 21:15:44 +0900

    feat: Home에 네비게이션 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,21 @@
+import { Link } from 'react-router-dom';
+import { ROUTER_PATH } from '../router';
+
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold">홈페이지</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">홈페이지</h1>
+      <ul>
+        <li className="text-blue-600 underline">
+          <Link to={ROUTER_PATH.KIOSK}>키오스트</Link>
+        </li>
+        <li className="text-blue-600 underline">
+          <Link to={ROUTER_PATH.ADMIN}>관리자</Link>
+        </li>
+        <li className="text-blue-600 underline">
+          <Link to={ROUTER_PATH.DISPLAY}>디스플레이</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
