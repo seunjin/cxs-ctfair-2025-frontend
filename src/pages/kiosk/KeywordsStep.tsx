9ca00b12c2a222001fedf45c346cd3d490609a34
seunjin <jinseun@gmail.com>
2025-09-10 15:30:06 +0900

    test : release

--- src/pages/kiosk/KeywordsStep.tsx
+++ src/pages/kiosk/KeywordsStep.tsx
@@ -36,6 +36,8 @@ const KeywordsStep = () => {
   const { data } = useQuery({
     queryKey: ['keywords'],
     queryFn: getKeywords,
+    refetchOnWindowFocus: false,
+    gcTime: 0,
   });
 
   const { mutate, isPending } = useMutation({
@@ -101,6 +103,17 @@ const KeywordsStep = () => {
             className="flex justify-center flex-wrap gap-5"
             labelClassName="text-[30px] tracking-[-0.01em]  h-[98px] px-[46px] rounded-[24px] outline outline-[3px]  outline-offset-[-3px] shadow-[0px_2px_10px_0px_rgba(0,0,0,0.25)]" // 라벨에 적용할 클래스
             options={styleKeywords}
+            // options={[
+            //   { value: '', label: '랜덤' },
+            //   { value: '', label: '네온' },
+            //   { value: '', label: '메탈' },
+            //   { value: '', label: '비대칭' },
+            //   { value: '', label: '모노톤' },
+            //   { value: '', label: '데님' },
+            //   { value: '', label: '럭셔리' },
+            //   { value: '', label: '스트릿' },
+            //   { value: '', label: '테크웨어' },
+            // ]}
           />
         </div>
 
@@ -115,6 +128,18 @@ const KeywordsStep = () => {
             className="flex justify-center flex-wrap gap-5"
             labelClassName="text-[30px] tracking-[-0.01em]  h-[98px] px-[46px] rounded-[24px] outline outline-[3px] outline-offset-[-3px] shadow-[0px_2px_10px_0px_rgba(0,0,0,0.25)]" // 라벨에 적용할 클래스
             options={moodKeywords}
+            // options={[
+            //   { value: '', label: '랜덤' },
+            //   { value: '', label: '몽환적인' },
+            //   { value: '', label: '미래적인' },
+            //   { value: '', label: '초현실주의' },
+            //   { value: '', label: '다크' },
+            //   { value: '', label: '팝아트' },
+            //   { value: '', label: '고딕' },
+            //   { value: '', label: '해체주의' },
+            //   { value: '', label: '실험적인' },
+            //   { value: '', label: '사이버펑크' },
+            // ]}
           />
         </div>
       </section>
