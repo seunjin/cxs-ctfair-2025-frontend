de250ecb3e810409bd0092de8a2ee0c4049627df
seunjin <jinseun@gmail.com>
2025-09-16 14:53:48 +0900

    choreL  캡쳐 로직 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // plugins: [react(), tailwindcss(), svgr(), basicSsl()],
   plugins: [react(), tailwindcss(), svgr()],
   server: {
+    host: true,
+    // https: true,
     proxy: {
       '/api': {
         target: 'https://api.cxsctfair.com',
